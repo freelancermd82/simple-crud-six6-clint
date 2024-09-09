feeb09514ae5de9db394571a05ca6b551e24b1f5
Your Name <freelancermd82@gmail.com>
2024-09-09 07:39:07 +0600

    update

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
@@ -30,7 +30,11 @@ const Users = () => {
             <div>
                 {
                     users.map(user => <p
-                        key={user._id}>{user.name} : {user.email} {user._id} <button
+                        key={user._id}>{user.name} : {user.email} {user._id}
+                        <Link to={`/update/${user._id}`}>
+                        <button>Update</button>
+                        </Link>
+                         <button
                             onClick={() => handleDelete(user._id)}
                         >X</button></p>)
                 }
